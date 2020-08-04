d5b9708dd363f3439b20a77a7e5676def20f4247
Markus-Rost <sukramxro@gmail.com>
2020-08-04 04:01:15 +0200

    save new translators from the optional strings

--- cmds/test.js
+++ cmds/test.js
@@ -14,9 +14,8 @@ function cmd_test(lang, msg, args, line, wiki) {
 		if ( msg.channel.type !== 'text' || !pause[msg.guild.id] ) this.LINK(lang, msg, line, wiki);
 	} else if ( msg.channel.type !== 'text' || !pause[msg.guild.id] ) {
 		if ( msg.isAdmin() && msg.defaultSettings ) help_setup(lang, msg);
-		let textList = lang.get('test.text').filter( text => text );
-		var text = textList[Math.floor(Math.random() * ( textList.length * 5 ))]?.trim();
-		if ( !text ) text = lang.get('test.text.0');
+		let textList = lang.get('test.text').filter( text => text.trim() );
+		var text = textList[Math.floor(Math.random() * ( textList.length * 5 ))] || lang.get('test.text.0');
 		console.log( '- Test: Fully functioning!' );
 		var now = Date.now();
 		msg.replyMsg( text ).then( message => {
