6b328a726f4db2820af347b245b0d96718a4eaba
TreeIsLife2 <71183248+TreeIsLife2@users.noreply.github.com>
2021-05-05 16:32:02 +0200

    Create help.js

---
+++ interactions/help.js
@@ -0,0 +1,17 @@
+var verify = require('../functions/help.js');
+const {sendMessage} = require('../util/functions.js');
+
+/**
+ * Post a message with inline wiki links.
+ * @param {Object} interaction - The interaction.
+ * @param {import('discord.js').Client} interaction.client - The client of the interaction.
+ * @param {import('../util/i18n.js')} lang - The user language.
+ * @param {import('../util/wiki.js')} wiki - The wiki for the interaction.
+ * @param {import('discord.js').TextChannel} [channel] - The channel for the interaction.
+ */
+
+function slash_inline(interaction, lang, channel) {
+  var text = "";
+  message.content = text;
+  return sendMessage();
+}
